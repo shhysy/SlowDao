7a45d487b77b6dc4e11419b1721b8755e2acc52e
shhysy <2157936243@qq.com>
2025-05-12 17:24:15 +0800

    auto: 升级 SlowDao.js 到 v1.19

--- SlowDao.js
+++ SlowDao.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SlowDao
 // @namespace    http://tampermonkey.net/
-// @version      1.18
+// @version      1.19
 // @description  Auto-updating userscript for SlowDao
 // @author       Your name
 // @match        *://*/*
