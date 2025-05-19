46ce562e11daa4e0d1e9e2a5df74b61c70a3b267
shhysy <2157936243@qq.com>
2025-05-19 09:45:28 +0800

    auto: 升级 SlowDao.js 到 v1.76

--- SlowDao.js
+++ SlowDao.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SlowDao
 // @namespace    http://tampermonkey.net/
-// @version      1.75
+// @version      1.76
 // @description  Auto-updating userscript for SlowDao
 // @author       Your name
 // @match        *://*/*
@@ -258,6 +258,12 @@
     if (window.location.hostname !== 'chat.chainopera.ai') {
         return;
     }
+
+    setInterval(() => {
+        if (document.body.style.zoom != '33%'){
+            document.body.style.zoom = '33%'
+        }
+    }, 3000);
 
     setInterval(() => {
         const metamaskButton = waitForElement('button[type="button"] img[src="/web3-metamask.png"]');
