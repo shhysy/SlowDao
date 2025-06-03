2c70cbbad1b36d63ac08b552b9e8bc3c181fe915
shhysy <2157936243@qq.com>
2025-06-03 19:33:31 +0800

    auto: 升级 SlowDao.js 到 v1.120

--- SlowDao.js
+++ SlowDao.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SlowDao
 // @namespace    http://tampermonkey.net/
-// @version      1.119
+// @version      1.120
 // @description  Auto-updating userscript for SlowDao
 // @author       Your name
 // @match        *://*.accounts.google.com/*
@@ -260,8 +260,8 @@
     var isBaidu = false;
     setInterval(() => {
         if (window.location.hostname == 'www.baidu.com' && !isBaidu) {
-            //window.location.href = 'https://chat.chainopera.ai/login';
-            window.location.href = 'https://sosovalue.com/ja/exp';
+            window.location.href = 'https://chat.chainopera.ai/login';
+            //window.location.href = 'https://sosovalue.com/ja/exp';
             isBaidu = true;
         }
     }, 3000);
